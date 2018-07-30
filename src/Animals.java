dc59a6d54bacca7d608d4068fe4158da119a6ffe
thaobui <buithaoiir@gmail.com>
2018-07-30 08:53:26 +0700

    tinh bmi va phan loai bmi

--- src/Animals.java
+++ src/Animals.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
 public class Animals {
     public static void main(String[] args) {
-        System.out.println("aaaa");
-
+        Scanner sc = new Scanner(System.in);
+        double weight;
+        double height;
+        double bmi;
+        System.out.print("Your weight (in kilograms): ");
+        weight = sc.nextDouble();
+        System.out.print("Your height (in meters: ");
+        height = sc.nextDouble();
+        bmi = weight / Math.pow(height, 2);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+        if (bmi < 18) {
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+        } else if (bmi < 25.0) {
+            System.out.printf("%-20.4f%s", bmi, "Normal");
+        } else if (bmi < 30.0) {
+            System.out.printf("%20.2f%s", bmi, "Overweight");
+        } else {
+            System.out.printf("%20.2f%s", bmi, "Obese");
+        }
     }
+
 }
